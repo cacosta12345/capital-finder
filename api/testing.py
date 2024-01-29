7acf4d309ee484797b1d0f931af2362bd8aa900e
chris acosta <chrisacosta12345@gmail.com>
2024-01-29 12:20:19 -0800

    adds testing.py

--- api/testing.py
+++ api/testing.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import platform
  
 class handler(BaseHTTPRequestHandler):
  
@@ -10,14 +11,14 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        # dict make a dictionary for key value pairs in param {"key":"value"}
-
         name = dic.get("name")
 
         if name:
             message = f"Hello {name}"
         else:
             message = "Hello stranger"
+
+        
 
         self.send_response(200)
         self.send_header('Content-type','text/plain')
