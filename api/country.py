28ad497ceb5bcc38d1dff8f386000fb0954cf1c2
chris acosta <chrisacosta12345@gmail.com>
2024-01-30 15:45:46 -0800

    fixed

--- api/country.py
+++ api/country.py
@@ -12,6 +12,8 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
+        message = ""
+
         if "country" in dic:
             url = "https://restcountries.com/v3.1/name/"
             r = requests.get(url + dic["country"])
@@ -22,10 +24,7 @@ class handler(BaseHTTPRequestHandler):
                 message = f"The capital of {country_name} is {capital_name}."
             else:
                 message = "No data found for the given country."
-        else:
-            message = "Please provide a valid country name."
-
-        if "capital" in dic:
+        elif "capital" in dic:
             url = "https://restcountries.com/v3.1/capital/"
             r = requests.get(url + dic["capital"])
             data = r.json()
@@ -36,7 +35,7 @@ class handler(BaseHTTPRequestHandler):
             else:
                 message = "No data found for the given capital."
         else:
-            message = "Please provide a valid capital name."
+            message = "Please provide a valid country name or capital name."
 
         
 
